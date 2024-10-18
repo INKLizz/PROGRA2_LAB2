d2e1806be2726fb6669b1ed6859dbf11acfb0d69
INKLizz <160358298+INKLizz@users.noreply.github.com>
2024-10-18 12:24:23 -0600

    push

---
+++ PROGRA2LAB2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package progra2lab2;
+
+/**
+ *
+ * @author Laura Sabillon
+ */
+public class PROGRA2LAB2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
